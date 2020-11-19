d6619d10ecdf5914a6c4e24abcfc24948056f1c4
haoboxuxu <haoboxuxu@icloud.com>
2020-11-20 00:46:55 +0800

    add scale/rotate time to sleep🌃

--- GameEngine/GameEngine/Display/Renderer.swift
+++ GameEngine/GameEngine/Display/Renderer.swift
@@ -22,6 +22,8 @@ extension Renderer: MTKViewDelegate {
         let commandBuffer = Engine.CommandQuque.makeCommandBuffer()
         let renderCommandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
         
+        player.update(deltaTime: 1 / Float(view.preferredFramesPerSecond))
+        
         player.render(renderCommandEncoder: renderCommandEncoder!)
         
         renderCommandEncoder?.endEncoding()
--- GameEngine/GameEngine/GameStuffs/GameObjects/GameObject.swift
+++ GameEngine/GameEngine/GameStuffs/GameObjects/GameObject.swift
@@ -7,16 +7,37 @@
 
 import MetalKit
 class GameObject: Node {
+    
+    var modelConstants = ModelConstants()
+    
     var mesh: Mesh!
+    
+    var time: Float = 0
     
     init(meshType: MeshTypes) {
         mesh = MeshLibrary.Mesh(meshType)
+    }
+    
+    func update(deltaTime: Float) {
+        time += deltaTime
+        
+        self.position.x = cos(time)
+        self.position.x = sin(time)
+        self.scale = float3(cos(time))
+        self.rotation.z = cos(time)
+        
+        updateModelConstants()
+    }
+    
+    private func updateModelConstants() {
+        modelConstants.modelMatrix = self.modelMatrix
     }
 }
 
 extension GameObject: Renderable {
     func doRender(_ renderCommandEncoder: MTLRenderCommandEncoder) {
         //renderCommandEncoder.setTriangleFillMode(.lines)
+        renderCommandEncoder.setVertexBytes(&modelConstants, length: ModelConstants.stride, index: 1)
         renderCommandEncoder.setRenderPipelineState(RenderPipelineStateLibrary.PipelineStates(.Basic))
         renderCommandEncoder.setVertexBuffer(mesh.vertexBuffer, offset: 0, index: 0)
         renderCommandEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: mesh.vertexCount)
