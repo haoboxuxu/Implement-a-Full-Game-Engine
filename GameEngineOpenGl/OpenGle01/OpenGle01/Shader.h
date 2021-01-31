4f787e8980a0cd9fdae98fb8ae0ce809067cc33f
徐浩博 <haoboxuxu@icloud.com>
2021-02-01 02:48:56 +0800

    GameEngineOpenGl:4

---
+++ GameEngineOpenGl/OpenGle01/OpenGle01/Shader.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class Shader
+{
+public:
+	Shader(const char* vertexPath, const char* fragmentPath);
+	string vertexString;
+	string fragmentString;
+	const char* vertexSource;
+	const char* fragmentSource;
+	unsigned int ID; // shader program id
+	void use();
+};
+
--- GameEngineOpenGl/OpenGle01/OpenGle01/main.cpp
+++ GameEngineOpenGl/OpenGle01/OpenGle01/main.cpp
@@ -2,39 +2,22 @@
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include "Shader.h"
 using namespace std;
 
 float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f,
+	-0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
+	 0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
+	 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,
 	 // 0.5f, -0.5f, 0.0f,
 	 // 0.0f,  0.5f, 0.0f,
-	 0.8f,  0.8f, 0.0f
+	 0.8f,  0.8f, 0.0f, 0.3f, 0.5f, 0.7f
 };
 
 unsigned int indices[] = {
 	0,1,2,
 	2,1,3
 };
-
-const char* vertexShaderSource = 
-"#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"out vec4 vertexColor;\n"
-"void main() {\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"	vertexColor = vec4(1.0, 0, 0, 1.0);\n"
-"}\0";
-
-const char* fragmentShaderSource =
-"#version 330 core\n"
-"in vec4 vertexColor;\n"
-"uniform vec4 ourColor;\n"
-"out vec4 FragColor;\n"
-"void main() {\n"
-"	FragColor = ourColor;\n"
-"} \n";
 
 void processInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
@@ -43,6 +26,7 @@ void processInput(GLFWwindow* window) {
 }
 
 int main() {
+
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -70,6 +54,8 @@ int main() {
 	// glCullFace(GL_BACK);
 	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
+	Shader* shader = new Shader("vertextSource.txt", "fragmentSource.txt");
+
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
@@ -84,24 +70,12 @@ int main() {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-
-	unsigned int shaderProgram;
-	shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	// render loop
 	while (!glfwWindowShouldClose(window)) {
@@ -113,13 +87,8 @@ int main() {
 		glBindVertexArray(VAO);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
-		float timeValue = glfwGetTime();
-		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-		glUseProgram(shaderProgram);
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+		shader->use();
 
-		glUseProgram(shaderProgram);
 		// glDrawArrays(GL_TRIANGLES, 0, 6);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
